673489fb33cab3f75cc5d3c2ec130e950587185c
Ihor <ihor@>
2023-12-12 17:39:05 +0200

    fix home task N5

---
+++ Product.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product {
+    public function get($name) {}
+    public function set($name, $value) {}
+}
+
+
+class ProcessProduct{
+    public function save(Product $product) {}
+    public function update(Product $product) {}
+    public function delete(Product $product) {}
+
+}
+class ViewProduct{
+    public function show(Product $product) {}
+    public function print(Product $product) {}
+}
+
